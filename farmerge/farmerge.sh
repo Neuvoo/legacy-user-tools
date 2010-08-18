3f12eda3cf1b4988e58bdb57a7f72d5b499d1886
Jacob Godserv <jacobgodserv@gmail.com>
2010-08-18 02:06:17 -0400

    Rewriting to be extremely modular. Halfway done, sort of.

--- farmerge/farmerge.sh
+++ farmerge/farmerge.sh
@@ -1,93 +1,57 @@
 #!/bin/bash
 
 function usage() {
-	echo "Usage: $(basename $0) [build|remove|shell|force-unlock|setup-chroot] --device <device name> [--cpv <[category/]package[-version]>] [--tarball /path/to/stage5-tarball] [--ask] [--once]"
+	echo
+	echo "Usage: $(basename $0) --device <device name> -- <module name> [<module arg> ...]"
+	echo
+	echo "List of modules:"
+	module_list "${MODULES_BASEDIR}" | while read module; do
+		echo " * ${module}"
+	done
 }
 
-basedir="$(dirname $0)"
-first_arg="1"
+export BASEDIR="$(dirname $0)"
+export CONF_BASEDIR="${BASEDIR}/conf"
+export LIB_BASEDIR="${BASEDIR}/lib"
+export MODULES_BASEDIR="${BASEDIR}/modules"
 
-unset action cpv device tarball extra_args
-extra_args=( )
+source "${LIB_BASEDIR}/modules"
+
+unset module args device
+args=( )
 while [[ "$1" != "" ]]; do
-	if [[ "$1" == "--cpv" ]]; then
-		if [[ "$2" == "" ]]; then
-			echo "--cpv requires an argument, but none given"
+	if [[ "${module}" == "" ]]; then
+		if [[ "$1" == "--device" ]]; then
+			device="${2}"
+			shift
+		elif [[ "$1" == "--" ]]; then
+			module="${2}"
+			shift
+		else
+			echo "Error: argument not understood: ${1}"
 			usage
 			exit 255
 		fi
-		cpv="$2"
-		shift
-	elif [[ "$1" == "--device" ]]; then
-		if [[ "$2" == "" ]]; then
-			echo "--device requires an argument, but none given"
-			usage
-			exit 255
-		fi
-		device="$2"
-		shift
-	elif [[ "$1" == "--tarball" ]]; then
-		if [[ "$2" == "" ]]; then
-			echo "--tarball requires an argument, but none given"
-			usage
-			exit 255
-		fi
-		tarball="$2"
-		shift
-	elif [[ "$1" == "--ask" ]]; then
-		extra_args+=( --ask )
-	elif [[ "$1" == "--once" ]]; then
-		extra_args+=( --once )
-	elif [[ "${first_arg}" == "1" ]]; then
-		action="${1}"
 	else
-		echo "argument not understood: $1"
-		usage
-		exit 255
+		args+=( "${1}" )
 	fi
-	first_arg=""
 	shift
 done
 
-if [[ "${action}" == "" ]]; then
-	action="build"
-	echo "action empty, defaulting to ${action}"
-fi
 if [[ "${device}" == "" ]]; then
-	echo "--device required"
+	echo "Error: --device must be specified"
 	usage
 	exit 255
 fi
-if [[ "${cpv}" == "" ]]; then
-	cpv="world"
-	echo "cpv empty, defaulting to ${cpv}"
-fi
-
-source "${basedir}/config" || exit $?
-
-if [[ "${action}" == "build" || "${action}" == "remove" || "${action}" == "shell" ]]; then
-	if [[ ! -d "${build_dir}" ]]; then
-		echo "Error: chroot directory doesn't exist: ${build_dir}"
-		echo "Run '$(basename $0) setup-chroot' to create this."
-		exit 2
-	fi
-	if [[ "${action}" == "build" ]]; then
-		action="add"
-	fi
-	"${basedir}/modules/build" --action "${action}" --cpv "${cpv}" --build-dir "${build_dir}" --staging-ssh-uri "${staging_ssh_uri}" --staging-ssh-path "${staging_ssh_path}" --mirror-ssh-uri "${mirror_ssh_uri}" --mirror-ssh-path "${mirror_ssh_path}" --mirror-http-uri "${mirror_http_uri}" --lock-ssh-uri "${lock_ssh_uri}" --lock-ssh-path "${lock_ssh_path}" "${extra_args[@]}"
-elif [[ "$action" == "force-unlock" ]]; then
-	source "${basedir}/modules/lib"
-	unlock_mirror "${lock_ssh_uri}" "${lock_ssh_path}"
-elif [[ "$action" == "setup-chroot" ]]; then
-	if [[ ! -n "${tarball}" ]]; then
-		echo "--tarball is required for setup-chroot, but none given"
-		usage
-		exit 255
-	fi
-
-	"${basedir}/modules/setup-chroot" --build-dir "${build_dir}" --tarball "${tarball}" --tarball-is-stage5 --timezone-path "${timezone_path}"
-else
-	echo "action not understood: ${action}"
+if [[ "${module}" == "" ]]; then
+	echo "Error: a module must be specified"
 	usage
 	exit 255
 fi
+
+export DEVICE="${device}"
+export DEVICE_BASEDIR="${basedir}/${device}"
+
+source "${CONF_BASEDIR}/global.config" || echo "Warning: failed to load global configuration!"
+
+"${MODULES_BASEDIR}/_exec-module" "${module}" "${args[@]}" || exit $?
